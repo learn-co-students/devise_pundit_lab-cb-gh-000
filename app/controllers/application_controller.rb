db1160e68e225d1f801067e048767aa48ad02c89
platrol <takialog@gmail.com>
2018-03-26 19:02:45 +0800

    pass test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include Pundit
+  helper_method :current_user
+  rescue_from Pundit::NotAuthorizedError do
+    flash[:error] = "Access denied."
+    redirect_to(request.referrer || root_path)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+
+  # :confirmable, :lockable, :timeoutable and :omniauthable, :confirmable,
+  # :recoverable, :trackable,
+  enum role: [:user, :moderator, :admin]
+
+  devise :database_authenticatable, :registerable, :validatable, :rememberable
   has_many :notes
   has_many :viewers
   has_many :readable, through: :viewers, source: :note
+
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
+
+
